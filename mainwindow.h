1387382f3a73e9291fa2b0671b8ce039dd6aa83a
pokeghost <pokeghost@disroot.org>
2025-08-27 18:46:01 +0300

    Init

---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+
+private:
+    Ui::MainWindow *ui;
+
+    int currentSeconds;
+    int maxSeconds;
+    bool isRunning;
+
+    void updateTimeDisplay();
+};
+#endif // MAINWINDOW_H
